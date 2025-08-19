12e166e18cd4d963f952f7bcc76eb585811f79d8
Victor <cucusise@gmail.com>
2025-08-19 16:43:49 +0200

    Cypher method created

--- rsa.py
+++ rsa.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def prime_generator():
     while True:
@@ -15,13 +16,32 @@ def prime_generator():
 
 
 
-def ASCII_conv(string):
-    conversion = []
-    for char in string:
-        conversion.append(ord(char))
-    return conversion
-
-
 def Euler_function(p, q):
     result = (p-1) * (q - 1)
-    return print(result)
+    return result
+
+
+def find_e(Euler):
+    while True:
+        e = random.randint(2, Euler - 1)
+        if math.gcd(e, Euler) == 1:
+            return e
+
+def n(p, q):
+    n = p * q
+    return n
+
+
+def cypher(plaintext, e, n):
+    plist = []
+    cypher = ""
+    for m in plaintext:
+        m = ord(m)
+        plist.append((m**e)%n)
+    return(plist)
+
+
+p = prime_generator()
+q = prime_generator()
+print(cypher("Hola", find_e(Euler_function(p, q)), n(p,q)))
+
