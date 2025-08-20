85c41bd65a16f7d9ca7e63c4c7b1f9ee20e78e50
Victor <cucusise@gmail.com>
2025-08-20 12:48:14 +0200

    UI implementation

--- rsa.py
+++ rsa.py
@@ -1,5 +1,6 @@
 import random
 import math
+import tkinter
 
 def prime_generator():
     while True:
@@ -13,7 +14,6 @@ def prime_generator():
 
         if prime:
             return number
-
 
 
 def Euler_function(p, q):
@@ -58,9 +58,51 @@ e = find_e(Euler)
 n = n(p,q)
 
 
-tcypher=cypher("Hello World!",e, n)
-print(tcypher)
+window = tkinter.Tk()
+window.title("Generador de xifrats RSA")
 
-tdecipher = decipher(tcypher, e, Euler, n)
-print(tdecipher)
 
+entrada = tkinter.Entry(window, font=("Arial", 40), width=25)
+entrada.insert(0, "Introdueix el text a encritar: ")
+entrada.grid(row = 0, column=0,padx=5, pady=5, columnspan=4)
+
+
+
+Column0 = tkinter.Label(text="Opcions per Xifrar")
+Column0.grid(row=1, column=0)
+
+Column0 = tkinter.Label(text="Opcions per desxifrar")
+Column0.grid(row=1, column=1)
+
+Column0 = tkinter.Label(text="Recursos")
+Column0.grid(row=1, column=2)
+
+
+#Columna 0
+genprim = tkinter.Button(text="Generar p i q")
+genprim.grid(row=2, column=0)
+
+genn = tkinter.Button(text="Generar nombre n")
+genn.grid(row=3, column=0)
+
+genEuler = tkinter.Button(text="Generar nombre funció Phi")
+genEuler.grid(row=4, column=0)
+
+startconv = tkinter.Button(text="Xifrar")
+startconv.grid(row=5, column=0)
+
+#Columna 1
+findd = tkinter.Button(text="Trobar d")
+findd.grid(row=2, column=1)
+
+startdesconv = tkinter.Button(text="Desxifrar")
+startdesconv.grid(row=3, column=1)
+
+#Columna 2
+astable = tkinter.Button(text="Taula de conversió ASCII")
+astable.grid(row=2, column=2)
+
+theory = tkinter.Button(text="Manual de teoria RSA")
+theory.grid(row=3, column=2)
+
+window.mainloop()
